b2f85aeb74dff333c9de987f4429994f0ff9c729
akilat90 <akila.thalgahagoda@gmail.com>
2018-10-05 12:51:18 +0530

    use autopep8 to format

--- server.py
+++ server.py
@@ -11,8 +11,9 @@ import tensorflow as tf
 app = flask.Flask(__name__)
 model = None
 
+
 def load_models():
-    
+
     global model, scaler, graph
 
     # model and weight paths
@@ -28,58 +29,59 @@ def load_models():
     model.load_weights(weights_path)
 
     # compile the model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics=['acc'])
 
     # load scaler
-    scaler_path='models/scaler.pickle'
+    scaler_path = 'models/scaler.pickle'
     scaler = joblib.load(scaler_path)
-    
+
     graph = tf.get_default_graph()
 
 
-
 def make_me_acceptable(sample):
-    
+
     # normalize the numeric columns
     sample_normalized = eh.normalize_numeric_columns(sample,
-                             columns=[c for c in sample.columns if (not c.endswith('_cat'))],
-                             scaler=scaler, 
-                             is_production=True
-                            )
-    cat_names = ['workclass_cat', 
-                'education_cat', 
-                'marital.status_cat', 
-                'occupation_cat', 
-                'relationship_cat', 
-                'race_cat', 
-                'sex_cat', 
-                'native.country_cat']
+                                                     columns=[c for c in sample.columns if (
+                                                         not c.endswith('_cat'))],
+                                                     scaler=scaler,
+                                                     is_production=True
+                                                     )
+    cat_names = ['workclass_cat',
+                 'education_cat',
+                 'marital.status_cat',
+                 'occupation_cat',
+                 'relationship_cat',
+                 'race_cat',
+                 'sex_cat',
+                 'native.country_cat']
 
     # make the input list
     input_list = eh.make_input_list(sample_normalized, cat_names)
 
     return input_list
 
-    
+
 @app.route("/predict", methods=["POST"])
 def predict():
-    
+
     # to be sent to the client
-    data = {"success": False}     
+    data = {"success": False}
 
     if flask.request.method == "POST":
-        
+
         try:
             # read file from request
             file = flask.request.files['file'].read()
-            
+
             # StringIO so that pandas can read it
-            file=StringIO(file)            
+            file = StringIO(file)
             sample = pd.read_csv(file)
-            
+
             # prepare the input list that the loaded model accepts
             input_list = make_me_acceptable(sample)
-            
+
             # load model ()
             with graph.as_default():
                 prediction = model.predict(input_list)
@@ -99,13 +101,8 @@ def predict():
     # return a json serialized version of data
     return flask.json.dumps(data)
 
+
 if __name__ == "__main__":
-    	print(("* Loading models and starting server..."))
-	load_models()
-	app.run()
-
-    
-        
-        
-
-
+    print(("* Loading models and starting server..."))
+    load_models()
+    app.run()
